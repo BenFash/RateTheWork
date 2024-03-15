4112285c9260e469f923ed019898dec1c2cd7bfa
Ben Fashan <benfashan71@msn.com>
2024-03-15 16:03:01 +0000

    ADD: margin top to works cards

--- home/views.py
+++ home/views.py
@@ -1,26 +1,34 @@
-from django.shortcuts import render, get_object_or_404, reverse
-from django.views import generic
-from django.contrib import messages
-from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render
 from django.db.models import Q
 from blog.models import Work
 
 def Index(request):
     # Searchbar functionality
     query = request.GET.get('q')
-    queryset = Work.objects.all()
+    queryset = Work.objects.filter(approved=True).order_by("-created_on")
 
     if query:
         queryset = queryset.filter(
             Q(title__icontains=query) |
             Q(content__icontains=query) |
             Q(sub_category__icontains=query) |
-            Q(categories__name__icontains=query) |  
-            Q(user__username__icontains=query)
+            Q(categories__name__icontains=query) | 
+            Q(user__username__icontains=query) 
         )
+
+    # Pagination
+    paginator = Paginator(queryset, 4) 
+    page_number = request.GET.get('page')
+    try:
+        object_list = paginator.page(page_number)
+    except PageNotAnInteger:
+        object_list = paginator.page(1)
+    except EmptyPage:
+        object_list = paginator.page(paginator.num_pages)
 
     context = {
         'query': query,
-        'object_list': queryset,
+        'object_list': object_list,
     }
     return render(request, 'home/index.html', context)

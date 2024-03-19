ca4b4843bf5bcc0a15b82b9c3a9a081e56242376
Ben Fashan <benfashan71@msn.com>
2024-03-19 12:21:34 +0000

    MAIT: turned debug false for heroku

--- rate_the_work/settings.py
+++ rate_the_work/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1','8000-benfash-ratethework-prmaj95y6lk.ws-eu108.gitpod.io',]
 
